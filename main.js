e5247e2b52079a9c7053b47f0f54f25eb972e5ae
singsong19 <69883408+singsong19@users.noreply.github.com>
2020-12-29 21:56:46 +0900

    Add files via upload
    Event-delegation
    - 부모요소에 이벤트 위임하기.

--- main.js
+++ main.js
@@ -15,38 +15,22 @@ const addBtn = document.querySelector('.footer__button');
         input.value ='';
         input.focus();
     }
-
+    let id = 0; //실제에서는 UUID, 해쉬코드등을 이용할것.
     function createItem(text) {
         const itemRow = document.createElement('li')
         itemRow.setAttribute('class', 'item__row');
-
-        const item = document.createElement('div')
-        item.setAttribute('class', 'item');
-
-        const name = document.createElement('span')
-        name.setAttribute('class', 'item__name');
-        name.innerText = text;
-
-        const deleteBtn = document.createElement('button');
-        deleteBtn.setAttribute('class', 'item__delete');
-        deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
-
-        deleteBtn.addEventListener('click', () =>{
-            items.removeChild(itemRow);
-        })
-
-        const itemDivider = document.createElement('div')
-        itemDivider.setAttribute('class', 'item__divider');
-
-        item.appendChild(name);
-        item.appendChild(deleteBtn);
-        
-        itemRow.appendChild(item);
-        itemRow.appendChild(itemDivider);
+        itemRow.setAttribute('data-id', id)
+            itemRow.innerHTML = `
+            <div class="item">
+                <span class="item__name">${text}</span>
+                <button class="item__delete">
+                    <i class="fas fa-trash" data-id=${id}></i>
+                </button>
+            </div>
+            <div class="item__divier"></div>
+            `;
+        id++;
         return itemRow;
-        
-
-
     } 
     
     addBtn.addEventListener('click', () =>{
@@ -58,3 +42,12 @@ const addBtn = document.querySelector('.footer__button');
             onAdd()
         }
     }) ;
+
+    //event-delegation
+    items.addEventListener('click', event =>{
+        const id = event.target.dataset.id;
+        if(id) {
+            const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+            toBeDeleted.remove();
+        }
+    })
